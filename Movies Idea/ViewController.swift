4ac7aace3e841ce2b5f7ab0f76801928fb50b9f4
richard mazid <richard.mazid@gmail.com>
2021-05-05 19:13:07 +0200

    Initial Commit

---
+++ Movies Idea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies Idea
+//
+//  Created by Richard Arif Mazid on 05/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
