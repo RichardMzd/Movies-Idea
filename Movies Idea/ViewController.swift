e8964896618a31e000a499b4e3d27a269ecfb6a5
richard mazid <richard.mazid@gmail.com>
2021-05-05 22:20:10 +0200

    Movies Idea trial

--- Movies Idea/ViewController.swift
+++ Movies Idea/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// First : Need an User Interface (UI)
+// Second : Need a network request to get our movie list
+// Third : Something to see info about movie
 
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var table: UITableView!
+    @IBOutlet var field: UITextField!
+    
+    var movies = [Movie]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+        field.delegate = self
+        
     }
 
+    // Field
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        moviesSearcher()
+        return true
+    }
+    
+    func moviesSearcher() {
+        field.resignFirstResponder()
+        
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+    }
+    
+    // Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Show movie details
+    }
+}
 
+
+struct Movie {
+    
 }
 
